4e6e6e7eee8570ea93425db232aa7daba532adfe
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2026-05-20 19:54:00 +0200

    toUnicode(): Support arrow operators

--- unicalc.js
+++ unicalc.js
@@ -186,7 +186,7 @@ function toASCII (unicode)
 }
 
 var rxASCII2UC = jsx.regexp.RegExp(
-  "(?<operator>(!=|:=|=:|\\^!|[-*/])|(?:\\\\|\\b)(?<operator-macro>approx|neq?|int|(?:not ?)?in|sum))"
+  "(?<operator>(!=|:=|=:|<?-->|<--|\\|->|<?==>|<==|\\^!|[-*/])|(?:\\\\|\\b)(?<operator-macro>approx|neq?|int|(?:not ?)?in|sum))"
   + '|\\\\(?<blackboard>[' + Object.keys(blackboard_map).join('') + '])'
   + "|\\b(?<root>(sq|cub)rt)\\b"
   + "|\\^(?<superscript>\\{(?<superscript-in-braces>.+?)\\}|(?<superscript-standalone>\\S+))"
@@ -222,6 +222,13 @@ function toUnicode (ascii)
           '!=': '≠',
           ':=': '≔',
           '=:': '≕',
+          '-->': '→',
+          '<--': '←',
+          '<-->': '↔',
+          '|->': '↦',
+          '==>': '⇒',
+          '<==': '⇐',
+          '<==>': '⇔',
           '^!': 'ᵎ',
           "-": "−",
           "*": "×",
